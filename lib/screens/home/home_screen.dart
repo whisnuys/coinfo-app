82449e69b429d2d319789dcbc5e63e51b2499239
whisnuys <whisnusaputra30@gmail.com>
2022-09-29 10:58:35 +0700

    adding bloc consumer homescreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,29 @@
-import 'package:coinfo_app/widgets/crypto_tile.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:io' show Platform;
+import '../../cubit/gainers_crypto_cubit.dart';
+import '../../models/coin_model.dart';
 import '../../constant/theme.dart';
-import '../../widgets/crypto_card_portfolio.dart';
+import '../../widgets/crypto_card_gainer.dart';
+import '../../widgets/filter_content.dart';
+import '../search_delegate.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    context.read<GainersCryptoCubit>().fetchGainersCrypto(6);
+    super.initState();
+  }
 
   Widget header() {
     return Container(
@@ -25,162 +44,21 @@ class HomeScreen extends StatelessWidget {
           const Spacer(),
           IconButton(
             splashRadius: 20,
-            onPressed: () {},
+            onPressed: () {
+              showSearch(context: context, delegate: SearchCrypto());
+            },
             icon: Image.asset(
               'assets/icon_search.png',
               width: 24,
               height: 24,
             ),
           ),
-          IconButton(
-            splashRadius: 20,
-            onPressed: () {},
-            icon: Image.asset(
-              'assets/icon_scan.png',
-              width: 24,
-              height: 24,
-            ),
-          ),
         ],
       ),
     );
   }
 
-  Widget cardBalance() {
-    return Container(
-      width: double.infinity,
-      height: 198,
-      padding: EdgeInsets.all(defaultMargin),
-      margin: EdgeInsets.only(
-        top: 30,
-        left: defaultMargin,
-        right: defaultMargin,
-      ),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage(
-            'assets/image_card.png',
-          ),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Your current balance',
-                style: whiteTextStyle.copyWith(
-                  fontWeight: semiBold,
-                  fontSize: 16,
-                ),
-              ),
-              Container(
-                width: 24,
-                height: 24,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/icon_eye.png',
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
-            '\$385,142',
-            style: whiteTextStyle.copyWith(
-              fontWeight: bold,
-              fontSize: 24,
-            ),
-          ),
-          const SizedBox(
-            height: 29,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 8),
-                    width: 24,
-                    height: 24,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/icon_upload.png',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Deposit',
-                    style: whiteTextStyle.copyWith(
-                      fontWeight: semiBold,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 8),
-                    width: 24,
-                    height: 24,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/icon_download.png',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Withdraw',
-                    style: whiteTextStyle.copyWith(
-                      fontWeight: semiBold,
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(bottom: 8),
-                    width: 24,
-                    height: 24,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/icon_history.png',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'History',
-                    style: whiteTextStyle.copyWith(
-                      fontWeight: semiBold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget myPortfolioTitle() {
+  Widget gainersTitle() {
     return Container(
       margin: EdgeInsets.only(
         top: 30,
@@ -191,22 +69,18 @@ class HomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            'My Portfolio',
+            'Top Gainers',
             style: blackTextStyle.copyWith(
               fontSize: 18,
               fontWeight: semiBold,
             ),
           ),
-          Text(
-            'See all',
-            style: primaryTextStyle,
-          ),
         ],
       ),
     );
   }
 
-  Widget myPortfolio() {
+  Widget gainers(List<CoinModel> gainerCoins) {
     return Container(
       margin: EdgeInsets.only(
         top: defaultMargin,
@@ -214,178 +88,17 @@ class HomeScreen extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: [
-            SizedBox(
-              width: defaultMargin,
-            ),
-            const CryptoCardPortfolio(
-              cryptoName: 'BNB',
-              logoUrl: 'assets/logo_bnb.png',
-              amountBalance: '\$47.99',
-              amountCrypto: 'BNB 0.095203',
-            ),
-            const CryptoCardPortfolio(
-              cryptoName: 'ETH',
-              logoUrl: 'assets/logo_eth.png',
-              amountBalance: '\$92.345',
-              amountCrypto: 'ETH 0.195203',
-            ),
-            SizedBox(
-              width: defaultMargin,
-            ),
-          ],
+          children: gainerCoins.map((coin) {
+            return Row(
+              children: [
+                SizedBox(
+                  width: defaultMargin,
+                ),
+                CryptoCardGainer(coin: coin),
+              ],
+            );
+          }).toList(),
         ),
-      ),
-    );
-  }
-
-  Widget tradeCryptoTitle() {
-    return Container(
-      margin: EdgeInsets.only(
-        top: 30,
-        left: defaultMargin,
-        right: defaultMargin,
-      ),
-      child: Text(
-        'Trade Crypto',
-        style: blackTextStyle.copyWith(
-          fontSize: 18,
-          fontWeight: semiBold,
-        ),
-      ),
-    );
-  }
-
-  Widget tradeCryptoFilter() {
-    return Container(
-      margin: const EdgeInsets.only(top: 24),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            SizedBox(
-              width: defaultMargin,
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 12),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              height: 33,
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: kPrimaryColor.withOpacity(0.5),
-                    spreadRadius: 0.5,
-                    blurRadius: 5,
-                    offset: const Offset(0, 2),
-                  ),
-                ],
-              ),
-              child: Center(
-                child: Text(
-                  'Name',
-                  style: whiteTextStyle.copyWith(
-                    fontWeight: semiBold,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 12),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              height: 33,
-              decoration: BoxDecoration(
-                color: kBackgroundColor2,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  '24h',
-                  style: blackTextStyle,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 12),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              height: 33,
-              decoration: BoxDecoration(
-                color: kBackgroundColor2,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  'My Portfolio',
-                  style: blackTextStyle,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(right: 12),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-              height: 33,
-              decoration: BoxDecoration(
-                color: kBackgroundColor2,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Text(
-                  'Market Cap',
-                  style: blackTextStyle,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget tradeCryptoTile() {
-    return Container(
-      margin: EdgeInsets.only(
-        top: defaultMargin,
-        left: defaultMargin,
-        right: defaultMargin,
-        bottom: 50,
-      ),
-      child: Column(
-        children: const [
-          CryptoTile(
-            logoUrl: 'assets/logo_bnb.png',
-            cryptoName: 'Binance',
-            cryptoNameExt: 'BNB',
-            price: '528.373',
-            percentage: '+1.82%',
-          ),
-          CryptoTile(
-            logoUrl: 'assets/logo_btc.png',
-            cryptoName: 'Bitcoin',
-            cryptoNameExt: 'BTC',
-            price: '33,257.78',
-            percentage: '+2.35%',
-          ),
-          CryptoTile(
-            logoUrl: 'assets/logo_eth.png',
-            cryptoName: 'Ethereum',
-            cryptoNameExt: 'ETH',
-            price: '954.221',
-            percentage: '+1.82%',
-          ),
-        ],
       ),
     );
   }
@@ -398,12 +111,39 @@ class HomeScreen extends StatelessWidget {
         child: ListView(
           children: [
             header(),
-            cardBalance(),
-            myPortfolioTitle(),
-            myPortfolio(),
-            tradeCryptoTitle(),
-            tradeCryptoFilter(),
-            tradeCryptoTile(),
+            gainersTitle(),
+            BlocConsumer<GainersCryptoCubit, GainersCryptoState>(
+              listener: (context, state) {
+                if (state is GainersCryptoLoadedFailed) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      backgroundColor: kRedColor,
+                      content: Text(
+                        state.error,
+                      ),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                if (state is GainersCryptoLoadedSuccess) {
+                  return gainers(state.gainerCoins);
+                }
+                return Center(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Platform.isAndroid
+                          ? const CircularProgressIndicator()
+                          : const CupertinoActivityIndicator(),
+                    ],
+                  ),
+                );
+              },
+            ),
+            const FilterContent(),
           ],
         ),
       ),
