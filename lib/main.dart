a4886e56c331aa7106948d9402b2c19406726e26
whisnuys <whisnusaputra30@gmail.com>
2022-09-29 10:57:05 +0700

    adding routes and bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:coinfo_app/api/coin_api.dart';
+import 'package:coinfo_app/cubit/crypto_cubit.dart';
+import 'package:coinfo_app/cubit/gainers_crypto_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
@@ -7,6 +11,7 @@ import 'screens/verify_email_screen.dart';
 import 'screens/setpin_screen.dart';
 import 'screens/congratulation_screen.dart';
 import 'screens/home/main_page.dart';
+import 'screens/detail_coin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,17 +22,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        "/": (context) => const SplashScreen(),
-        "/register": (context) => const RegisterScreen(),
-        "/login": (context) => const LoginScreen(),
-        "/verifemail": (context) => const VerifyEmailScreen(),
-        "/setpin": (context) => const SetpinScreen(),
-        "/congrats": (context) => const CongratulationScreen(),
-        "/main": (context) => const MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => CryptoCubit(),
+        ),
+        BlocProvider(
+          create: (context) => GainersCryptoCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          "/": (context) => const SplashScreen(),
+          "/register": (context) => const RegisterScreen(),
+          "/login": (context) => const LoginScreen(),
+          "/verifemail": (context) => const VerifyEmailScreen(),
+          "/setpin": (context) => const SetpinScreen(),
+          "/congrats": (context) => const CongratulationScreen(),
+          "/main": (context) => const MainPage(),
+        },
+      ),
     );
   }
 }
