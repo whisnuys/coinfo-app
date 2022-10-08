31cf5ad272f10a5619f732bbb00e8d0e5e58b053
whisnuys <whisnusaputra30@gmail.com>
2022-10-08 22:57:19 +0700

    add route name

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:coinfo_app/api/coin_api.dart';
 import 'package:coinfo_app/cubit/crypto_cubit.dart';
 import 'package:coinfo_app/cubit/gainers_crypto_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'screens/detail_news_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
@@ -11,7 +11,6 @@ import 'screens/verify_email_screen.dart';
 import 'screens/setpin_screen.dart';
 import 'screens/congratulation_screen.dart';
 import 'screens/home/main_page.dart';
-import 'screens/detail_coin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -41,6 +40,7 @@ class MyApp extends StatelessWidget {
           "/setpin": (context) => const SetpinScreen(),
           "/congrats": (context) => const CongratulationScreen(),
           "/main": (context) => const MainPage(),
+          "/detail-news": (context) => const DetailNewsScreen(),
         },
       ),
     );
